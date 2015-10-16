61c4307373ef40bf47baed70e7f131fab4369c2d
Chinthanie <chinny@hoyne.com.au>
2015-10-17 04:31:26 +1100

    images and animations
    Added images,
    added js animations

--- chinnys-folio/js/main.js
+++ chinnys-folio/js/main.js
@@ -1,21 +1,38 @@
-
 /*global $, console*/
 (function () {
     'use strict';
     $(document).ready(function () {
-        /* Find the hamburger li and attach a click handler */
+
+
+        /*DROP DOWN HAMBURGER MENU*/
         $('nav ul:first-child').click(function () {
-            /* toggle the class menu-expanded on the ul
-            ** be careful with the "this" context as our selector is a pseudo element and "this" refers to the ul
-            **/
             $(this).toggleClass('menu-expanded li');
-            
         });
-        
+
+
+        //        $('ul:last-child').click(function () {
+        //            $('nav ul:first-child').toggleClass('menu-expanded li');
+        //        });
+
+
+        //        var menu = false;
+        //        $('nav ul:first-child').click(function () {
+        //            if (!menu) {
+        //                $('ul:not(first-child)').animate({top: "80px"});
+        //                menu = true;
+        //            } else {
+        //                $("ul:not(first-child)").animate({top: "0px"});
+        //                menu = false;
+        //            }
+        //        });
+        //        
+
+
+
+
         /*SMOOTH SCROLL*/
         $('a[href*=#]:not([href=#])').click(function () {
-            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
-                    || location.hostname === this.hostname) {
+            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') || location.hostname === this.hostname) {
 
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -27,7 +44,24 @@
                 }
             }
         });
-    
-    
+
+
+        /*MOUSE OVER ANIMATION*/
+
+        $('.projectimage').mouseover(function () {
+            $(this).addClass('animated pulse');
+        });
+
+        $('.projectimage').mouseleave(function () {
+            $(this).removeClass('animated pulse');
+        });
+
+        $('.menu-icon').mouseover(function () {
+            $(this).addClass('animated flip');
+        });
+
+        $('.menu-icon').mouseleave(function () {
+            $(this).removeClass('animated flip');
+        });
     });
 }());
